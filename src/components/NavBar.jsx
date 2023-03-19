fb98faf026bd5b02f5423a09d1e8f4086ccf8646
Netflix Clone <dhanushhv75@gmail.com>
2023-03-19 12:21:59 +0530

    Made NavBar responsive to Mobile devices

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,8 @@
-import { HStack, Text, Box, Avatar } from "@chakra-ui/react"
-import { motion } from "framer-motion"
+import { HStack, Text, Menu, MenuButton, MenuList, MenuItem, IconButton } from "@chakra-ui/react"
+import { BiHomeAlt } from "react-icons/bi"
+import { CgMenuLeftAlt } from "react-icons/cg"
+import { MdOutlineHandshake, MdPerson, MdTimeline } from "react-icons/md"
+import { BiGitCommit } from "react-icons/bi"
 import { Link } from "react-router-dom"
 import React from "react"
 import "./NavBar.css"
@@ -22,12 +25,24 @@ const NavBar = () => {
 	return (
 		<HStack className="navbar" width="full" p={5} spacing={5} bg="blackAlpha.200" backdropFilter="blur(7px)" zIndex={1}>
 			<Link to="/" className="brand">Portfolio</Link>
-			<HStack spacing={5} justify="flex-end" width="full">
-				<Link id="navLink" to="/" onClick={() => setActiveWindow(0)}><Text color={homeFg}>Home</Text></Link>
-				<Link id="navLink" to="/about" onClick={() => setActiveWindow(1)}><Text color={aboutFg}>About</Text></Link>
-				<Link id="navLink" to="/projects" onClick={() => setActiveWindow(2)}><Text color={projectsFg}>Projects</Text></Link>
-				<Link id="navLink" to="/experience" onClick={() => setActiveWindow(3)}><Text color={expFg}>Experience</Text></Link>
-				<Link id="navLink" to="/contact" onClick={() => setActiveWindow(4)}><Text color={contactFg}>Contact</Text></Link>
+			<HStack w="full" justify="flex-end">
+				<HStack spacing={5} justify="flex-end" w="full" display={{ base: "none", md: "flex", lg: "flex" }}>
+					<Link id="navLink" to="/" onClick={() => setActiveWindow(0)}><Text color={homeFg}>Home</Text></Link>
+					<Link id="navLink" to="/about" onClick={() => setActiveWindow(1)}><Text color={aboutFg}>About</Text></Link>
+					<Link id="navLink" to="/projects" onClick={() => setActiveWindow(2)}><Text color={projectsFg}>Projects</Text></Link>
+					<Link id="navLink" to="/experience" onClick={() => setActiveWindow(3)}><Text color={expFg}>Experience</Text></Link>
+					<Link id="navLink" to="/contact" onClick={() => setActiveWindow(4)}><Text color={contactFg}>Contact</Text></Link>
+				</HStack>
+				<Menu>
+					<MenuButton as={IconButton} variant="ghost" icon={<CgMenuLeftAlt size="25px"/>} display={{ base: "flex", md: "none", lg: "none" }} />
+					<MenuList>
+						<Link to="/"><MenuItem icon={<BiHomeAlt size="25px" />}>Home</MenuItem></Link>
+						<Link to="/about"><MenuItem icon={ <MdPerson size="25px" />}>About</MenuItem></Link>
+						<Link to="/projects"><MenuItem icon={ <BiGitCommit size="25px" />}>Projects</MenuItem></Link>
+						<Link to="/experience"><MenuItem icon={ <MdTimeline size="25px" />}>Experience</MenuItem></Link>
+						<Link to="/contact"><MenuItem icon={ <MdOutlineHandshake size="25px" />}>Contact</MenuItem></Link>
+					</MenuList>
+				</Menu>
 			</HStack>
 		</HStack>
 	)
