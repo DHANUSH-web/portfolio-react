4f9188197b7e116e7befa0aec0f2240c3deb3bd1
DhanushHV <dhanushhv75@gmail.com>
2023-04-03 19:57:31 +0530

    Fixed the NavBar active window detection bug

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,22 +3,22 @@ import { MdOutlineHandshake, MdPerson, MdTimeline } from "react-icons/md"
 import { BiGitCommit, BiSun, BiMoon } from "react-icons/bi"
 import { CgMenuLeftAlt } from "react-icons/cg"
 import { BiHomeAlt } from "react-icons/bi"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import { motion } from "framer-motion"
 import React from "react"
 import "./NavBar.css"
 
 const NavBar = () => {
 	const { colorMode, toggleColorMode } = useColorMode()
+	const urlPath = useLocation()
+	const [ activeWindow, setActiveWindow ] = React.useState(urlPath.pathname)
+	const isHome = activeWindow === '/'
+	const isAbout = activeWindow === '/about'
+	const isProjects = activeWindow === '/projects'
+	const isExperience = activeWindow === '/experience'
+	const isContact = activeWindow === '/contact'
 	const isDark = colorMode === "dark"
-
-	const [ activeWindow, setActiveWindow ] = React.useState(0)
-	const isHome = activeWindow === 0
-	const isAbout = activeWindow === 1
-	const isProjects = activeWindow === 2
-	const isExperience = activeWindow === 3
-	const isContact = activeWindow === 4
-
+	
 	const homeFg = isHome ? (isDark) ? "aqua" : "#0050ff" : (isDark) ? "white" : "black"
 	const aboutFg = isAbout ? (isDark) ? "aqua" : "#0050ff" : (isDark) ? "white" : "black"
 	const projectsFg = isProjects ? (isDark) ? "aqua" : "#0050ff" : (isDark) ? "white" : "black"
@@ -31,11 +31,11 @@ const NavBar = () => {
 			<Link to="/" className="brand"><Text color={logoFg}>Portfolio</Text></Link>
 			<HStack w="full" justify="flex-end">
 				<HStack spacing={5} justify="flex-end" w="full" display={{ base: "none", md: "flex", lg: "flex" }}>
-					<Link id="navLink" to="/" onClick={() => setActiveWindow(0)}><Text color={homeFg}>Home</Text></Link>
-					<Link id="navLink" to="/about" onClick={() => setActiveWindow(1)}><Text color={aboutFg}>About</Text></Link>
-					<Link id="navLink" to="/projects" onClick={() => setActiveWindow(2)}><Text color={projectsFg}>Projects</Text></Link>
-					<Link id="navLink" to="/experience" onClick={() => setActiveWindow(3)}><Text color={expFg}>Experience</Text></Link>
-					<Link id="navLink" to="/contact" onClick={() => setActiveWindow(4)}><Text color={contactFg}>Contact</Text></Link>
+					<Link id="navLink" to="/" onClick={() => setActiveWindow('/')}><Text color={homeFg}>Home</Text></Link>
+					<Link id="navLink" to="/about" onClick={() => setActiveWindow('/about')}><Text color={aboutFg}>About</Text></Link>
+					<Link id="navLink" to="/projects" onClick={() => setActiveWindow('/projects')}><Text color={projectsFg}>Projects</Text></Link>
+					<Link id="navLink" to="/experience" onClick={() => setActiveWindow('/experience')}><Text color={expFg}>Experience</Text></Link>
+					<Link id="navLink" to="/contact" onClick={() => setActiveWindow('/contact')}><Text color={contactFg}>Contact</Text></Link>
 					<IconButton as={motion.button} whileTap={{ scale: 1.5 }} icon={ isDark ? <BiSun size="20px" /> : <BiMoon size="20px" /> } onClick={toggleColorMode} colorScheme="blue"/>
 				</HStack>
 				<Menu>
