cc0fa822dfeab1025fcff2f6ca239e78f5add83e
DhanushHV <dhanushhv75@gmail.com>
2023-05-05 01:00:51 +0530

    Update Profile card and Achievements Logo

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,17 +1,19 @@
-import { Button, Wrap, HStack, VStack, Card, CardHeader, CardBody, CardFooter, Text, Image } from "@chakra-ui/react"
+import { Button, Wrap, HStack, VStack, Card, CardHeader, CardBody, CardFooter, Text, Image, useColorMode } from "@chakra-ui/react"
+import { AchievementsDark, AchievementsLight } from "./FigmaDesigns"
 import data from "../database.json"
 import "./Achievements.css"
 import React from "react"
 
-
 const Achievements = () => {
+	const { colorMode } = useColorMode()
 	const profile = data.profiles[data.profileHolder]
 	const cards = profile.achievements
+	const isDark = colorMode === 'dark'
 
 	return (
 		<VStack mt={{ base: 100, lg: 70 }} spacing={15}>
-			<Text fontFamily="googleSansBold" fontSize={30}>Achievements</Text>
-			<Wrap pt={25} w="full" justify="center" display={{ base: "none", md: "grid", lg: "grid"}}>
+			{ isDark ? <AchievementsDark /> : <AchievementsLight /> }
+			<Wrap pt={25} w="full" justify="center" display={{ base: "none", md: "grid", lg: "grid" }}>
 				{
 					cards.map((card) => (
 						<Card className="cards" align="center" w={300} p={15} border="solid 2px" borderColor="blackAlpha.200" borderRadius={12}>
@@ -28,21 +30,21 @@ const Achievements = () => {
 								<Button className="view-btn" onClick={() => window.open(card.url)} w='200px' borderRadius={20} colorScheme="blue">View</Button>
 							</CardFooter>
 						</Card>
-						)
+					)
 					)
 				}
 			</Wrap>
-			<Wrap pt={25} w="full" align="center" justify="center" display={{ base: "flex", md: "none", lg: "none"}}>
+			<Wrap pt={25} w="full" align="center" justify="center" display={{ base: "flex", md: "none", lg: "none" }}>
 				{
 					cards.map((card) => (
-							<HStack className="shortCards" align="center">
-								<Image src={card.icon} h={20} borderRadius={7} />
-								<VStack w="full">
-									<Text textAlign="initial" fontSize={15} fontFamily="googleSansBold">{card.title}</Text>
-									<Button onClick={() => window.open(card.url)} fontFamily="googleSans" fontWeight={350} colorScheme="blue" w="full">View</Button>
-								</VStack>
-							</HStack>
-						)
+						<HStack className="shortCards" align="center">
+							<Image src={card.icon} h={20} borderRadius={7} />
+							<VStack w="full">
+								<Text textAlign="initial" fontSize={15} fontFamily="googleSansBold">{card.title}</Text>
+								<Button onClick={() => window.open(card.url)} fontFamily="googleSans" fontWeight={350} colorScheme="blue" w="full">View</Button>
+							</VStack>
+						</HStack>
+					)
 					)
 				}
 			</Wrap>
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,16 +1,21 @@
-import { Box, HStack, Stack, VStack, Button, Text, Image, StackDivider, Link } from "@chakra-ui/react"
+import { Box, HStack, Stack, VStack, Button, Text, Image, StackDivider, Link, useColorMode } from "@chakra-ui/react"
+import { ProfileNameLight, ProfileNameDark } from '../components/FigmaDesigns'
 import { MdLink, MdArrowForward } from "react-icons/md"
 import { Link as Hyper } from "react-router-dom"
 import { motion } from "framer-motion"
 import React from "react"
 
 const ProfileCard = ({ ...props }) => {
+	const {colorMode} = useColorMode()
+	const isDark = colorMode === 'dark'
+
 	return (
 		<>
 			<Box className="profile" align="center" w="full" p={{ base: 10, md: 20, lg: 20 }}>
 				<HStack align="center" justify="center" spacing={{ base: 0, lg: "10rem" }} display={{ base: "block", lg: "flex" }}>
-					<VStack spacing="none">
-						<Text className="profileName" fontSize={{ base: 50, lg: 70 }} fontFamily="googleSans">{props.profileName}</Text>
+					<VStack w={{ md: "500px", lg: '700px' }}>
+						{ props.textMode ? <Text className="profileName" fontSize={{ base: 50, lg: 70 }} fontFamily="googleSans">{props.profileName}</Text>
+						: isDark ? <ProfileNameDark /> : <ProfileNameLight /> }
 						<HStack divider={<StackDivider />}>
 							<Text to="/experience" as={Hyper} fontFamily="googleSans">{props.jobRole}</Text>
 							<Text as={Link} className="hyper-link" fontFamily="googleSansBold" color="blue.500" href={props.companyURL} target="_blank">{props.companyName}</Text>
