f3d95f040ce46ce8c4cca346dc3a2943e7597fe8
PortSol <web.portsol@gmail.com>
2024-06-17 22:39:52 +0530

    Update Achievements card user experience

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,6 +1,7 @@
-import { Button, Stack, Card, CardHeader, CardBody, CardFooter, Text, Image, useColorMode, Flex } from "@chakra-ui/react"
+import { Button, Card, CardHeader, CardBody, CardFooter, Text, Image, useColorMode, Flex } from "@chakra-ui/react"
 import { AchievementsDark, AchievementsLight } from "./FigmaDesigns"
 import { IconExternalLink } from "@tabler/icons-react"
+import { Link } from "react-router-dom"
 import data from "../database.json"
 import "./Achievements.css"
 import React from "react"
@@ -12,49 +13,52 @@ const Achievements = () => {
 	const isDark = colorMode === 'dark'
 
 	return (
-		<Stack direction='column' mt={{ base: 100, lg: 70 }} gap={12} p={5} align='center'>
+		<Flex direction='column' alignItems="center" gap={12} my={20} mx={5}>
 			{ isDark ? <AchievementsDark /> : <AchievementsLight /> }
-			<Flex flexWrap='wrap' justifyContent='center' gap={5} w="full" display={{ base: 'none', sm: 'none', md: 'flex', lg: 'flex' }}>
+			<Flex maxW={1024} flexWrap="wrap" justifyContent="center" gap={5} display={{base: "none", sm: "none", md: "flex", lg: "flex"}}>
 				{
 					cards.map((card, idx) => (
 						<Card key={"card__" + idx} w={400} borderRadius={20} variant="outline">
 							<CardHeader>
-								<Stack direction='row' spacing={5} align='center'>
+								<Flex alignItems="center" gap={5}>
 									<Image src={card.icon} h={20} borderRadius="full" />
-									<Text textAlign="initial" fontFamily="interSemiBold">{card.title}</Text>
-								</Stack>
+									<Text fontFamily="interSemiBold">{card.title}</Text>
+								</Flex>
 							</CardHeader>
 							<CardBody>
-								<Text textAlign="initial" fontFamily="interRegular" fontSize="md">{card.desc}</Text>
+								<Text fontFamily="interRegular">{card.desc}</Text>
 							</CardBody>
-							<CardFooter justify="end">
-								<Button
-									fontFamily="interSemiBold"
-									width={28}
-									rightIcon={<IconExternalLink size={15} />}
-									onClick={() => window.open(card.url)}
-								>
-									View
-								</Button>
+							<CardFooter justifyContent="end">
+								<Link to={card.url} target="_blank">
+									<Button
+										fontFamily="interSemiBold"
+										width={28}
+										rightIcon={<IconExternalLink size={12} />}
+									>
+										View
+									</Button>
+								</Link>
 							</CardFooter>
 						</Card>
 					))
 				}
 			</Flex>
-			<Flex flexWrap='wrap' gap={5} justifyContent='center' w="full" display={{ base: 'flex', sm: 'flex', md: 'none', lg: 'none'}}>
+			<Flex flexWrap="wrap" gap={5} justifyContent="center" display={{ base: 'flex', sm: 'flex', md: 'none', lg: 'none'}}>
 				{
 					cards.map((card) => (
-						<Flex key={"card__"+card.url} direction='row' width={250} gap={2} border="solid" borderColor="blackAlpha.200" borderRadius={12} shadow="lg" p={1}>
+						<Flex key={"card__"+card.url} gap={2} w={300} p={1} borderRadius={9} border="solid" borderColor={isDark ? "whiteAlpha.200" : "blackAlpha.100"} shadow="sm">
 							<Image src={card.icon} h={20} borderRadius={7} />
-							<Flex direction='column' justify="space-between" wrap="wrap" width="inherit">
+							<Flex direction='column' justifyContent="space-between" alignItems="end" w="full">
 								<Text textAlign="initial" fontSize={15} fontFamily="interSemiBold">{card.title}</Text>
-								<Button size="sm" onClick={() => window.open(card.url)} rightIcon={<IconExternalLink size={15} />}>View</Button>
+								<Link to={card.url}>
+									<Button width="fit" size="sm" rightIcon={<IconExternalLink size={15} />}>View</Button>
+								</Link>
 							</Flex>
 						</Flex>
 					))
 				}
 			</Flex>
-		</Stack>
+		</Flex>
 	)
 }
 
